82d721f4b503a9247a421cf34546430a0aec1e12
MTocci1 <Mason.Tocci1@marist.edu>
2023-09-28 16:59:28 -0400

    completed lab 3

--- GameProg1/Lab3/Bee.h
+++ GameProg1/Lab3/Bee.h
@@ -1,1 +1,26 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+
+class Bee
+{
+private:
+	// Center of the circle around which the bee will move
+	float circleCenterX = 450.0f;
+	float circleCenterY = 450.0f;
+	float radius = 200.0f;
+
+	// Set the initial angle
+	float angle = 0.0f;
+
+	bool m_MovingClockwise = true;
+	bool m_MovingCounterClockwise = false;
+
+public:
+	Bee(Texture& textureBee);
+	Sprite spriteBee;
+	void moveClockwise();
+	void moveCounterClockwise();
+	void update(Time dt);
+};
