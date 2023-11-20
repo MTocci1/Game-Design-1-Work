ee61cea01bfbd7ccbb390f251bfa0bbc68210577
MTocci1 <Mason.Tocci1@marist.edu>
2023-11-20 14:23:43 -0500

    completed chapter 14-15

--- GameProg1/Thomas Was Late/Draw.cpp
+++ GameProg1/Thomas Was Late/Draw.cpp
@@ -15,6 +15,9 @@ void Engine::draw()
 		// Switch to m_MainView
 		m_Window.setView(m_MainView);
 
+		// Draw the Level
+		m_Window.draw(m_VALevel, &m_TextureTiles);
+
 		// Draw thomas
 		m_Window.draw(m_Thomas.getSprite());
 
@@ -34,6 +37,9 @@ void Engine::draw()
 		// Switch to m_LeftView
 		m_Window.setView(m_LeftView);
 
+		// Draw the Level
+		m_Window.draw(m_VALevel, &m_TextureTiles);
+
 		// Draw bob
 		m_Window.draw(m_Bob.getSprite());
 
@@ -50,6 +56,9 @@ void Engine::draw()
 		// Switch to m_RightView
 		m_Window.setView(m_RightView);
 
+		// Draw the Level
+		m_Window.draw(m_VALevel, &m_TextureTiles);
+
 		// Draw thomas
 		m_Window.draw(m_Thomas.getSprite());
 
@@ -61,6 +70,13 @@ void Engine::draw()
 	// Switch to m_HudView
 	m_Window.setView(m_HudView);
 
+	m_Window.draw(m_Hud.getLevel());
+
+	m_Window.draw(m_Hud.getTime());
+	if (!m_Playing)
+	{
+		m_Window.draw(m_Hud.getMessage());
+	}
 
 	// Show everything we have just drawn
 	m_Window.display();
