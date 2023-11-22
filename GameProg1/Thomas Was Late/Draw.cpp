323e0d1f099be625e752f00b3987438c935a38b2
MTocci1 <Mason.Tocci1@marist.edu>
2023-11-22 10:59:00 -0500

    completed chapter 18

--- GameProg1/Thomas Was Late/Draw.cpp
+++ GameProg1/Thomas Was Late/Draw.cpp
@@ -4,13 +4,17 @@ void Engine::draw()
 {
 	// Rub out the last frame
 	m_Window.clear(Color::White);
+
+	// Update the shader parameters
+	m_RippleShader.setUniform("uTime", m_GameTimeTotal.asSeconds());
+
 	if (!m_SplitScreen)
 	{
 		// Switch to background view
 		m_Window.setView(m_BGMainView);
 
-		// Draw the background
-		m_Window.draw(m_BackgroundSprite);
+		// Draw the background, complete with shader effect
+		m_Window.draw(m_BackgroundSprite, &m_RippleShader);
 
 		// Switch to m_MainView
 		m_Window.setView(m_MainView);
@@ -23,6 +27,12 @@ void Engine::draw()
 
 		// Draw bob
 		m_Window.draw(m_Bob.getSprite());
+
+		// Draw the particle system
+		if (m_PS.running())
+		{
+			m_Window.draw(m_PS);
+		}
 	}
 	else
 	{
@@ -31,8 +41,8 @@ void Engine::draw()
 		// Switch to background view
 		m_Window.setView(m_BGLeftView);
 
-		// Draw the background
-		m_Window.draw(m_BackgroundSprite);
+		// Draw the background, complete with shader effect
+		m_Window.draw(m_BackgroundSprite, &m_RippleShader);
 
 		// Switch to m_LeftView
 		m_Window.setView(m_LeftView);
@@ -46,12 +56,18 @@ void Engine::draw()
 		// Draw thomas
 		m_Window.draw(m_Thomas.getSprite());
 
+		// Draw the particle system
+		if (m_PS.running())
+		{
+			m_Window.draw(m_PS);
+		}
+
 		// Now draw Bob's side of the screen
 		// Switch to background view
 		m_Window.setView(m_BGRightView);
 
-		// Draw the background
-		m_Window.draw(m_BackgroundSprite);
+		// Draw the background, complete with shader effect
+		m_Window.draw(m_BackgroundSprite, &m_RippleShader);
 
 		// Switch to m_RightView
 		m_Window.setView(m_RightView);
@@ -64,6 +80,12 @@ void Engine::draw()
 
 		// Draw bob
 		m_Window.draw(m_Bob.getSprite());
+
+		// Draw the particle system
+		if (m_PS.running())
+		{
+			m_Window.draw(m_PS);
+		}
 
 	}
 	// Draw the HUD
